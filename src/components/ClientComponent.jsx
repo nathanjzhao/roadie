c1a30af95f6f0f21c5a7a9df50e7451e3f6fb6d5
nathanjzhao <43712744+nathanjzhao@users.noreply.github.com>
2024-06-22 14:56:14 -0700

    working on configid

--- src/components/ClientComponent.jsx
+++ src/components/ClientComponent.jsx
@@ -6,8 +6,12 @@ import Controls from "./Messages";
 export default function ClientComponent({
   accessToken,
 }) {
+  console.log(process.env.NEXT_PUBLIC_HUME_CONFIG_ID)
   return (
-    <VoiceProvider auth={{ type: "accessToken", value: accessToken }}>
+    <VoiceProvider 
+      auth={{ type: "accessToken", value: accessToken }}
+      configId={process.env.NEXT_PUBLIC_HUME_CONFIG_ID}
+    >
       <Messages />
       <Controls />
     </VoiceProvider>
